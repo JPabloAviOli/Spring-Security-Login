23fa0afe50f55e9ac35025aa06e06cb8c8efb9c7
JPablo Avila <dev.web.jpao@gmail.com>
2024-09-03 22:13:48 -0600

    Agregando commanLineRunner

--- src/main/java/com/pavila/SecureLogin/SecureLoginApplication.java
+++ src/main/java/com/pavila/SecureLogin/SecureLoginApplication.java
@@ -1,8 +1,12 @@
 package com.pavila.SecureLogin;
 
 
+import com.pavila.SecureLogin.model.entity.security.Role;
+import com.pavila.SecureLogin.repository.security.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -15,5 +19,12 @@ public class SecureLoginApplication {
 		SpringApplication.run(SecureLoginApplication.class, args);
 	}
 
-
+	@Bean
+	CommandLineRunner commandLineRunner(RoleRepository roleRepository){
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(Role.builder().name("USER").build());
+			}
+		};
+	}
 }
